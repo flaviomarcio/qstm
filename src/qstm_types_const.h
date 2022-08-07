93cd71c9aefae41b2bf7a604016c959c9ca4fde7
Flavio Portela <flavio.portela@geofusion.com.br>
2022-08-07 02:34:20 -0300

    cleanup qstm const

--- src/qstm_types_const.h
+++ src/qstm_types_const.h
@@ -2,12 +2,7 @@
 
 #include <QObject>
 
-struct EditReadOnly{
-    const char *erNo="erNo";
-    const char *erYes="erYes";
-    const char *erIfNew="erIfNew";
-    const char *erIfExisting="erIfExisting";
-};
+
 
 static const auto vDELETE="delete";
 static const auto vGET="get";
@@ -15,115 +10,6 @@ static const auto vPATCH="patch";
 static const auto vPOST="post";
 static const auto vPUT="put";
 static const auto vTRACE="trace";
-
-static const auto vList="list";
-static const auto vObject="object";
-
-static const auto vActions="actions";
-static const auto vAuto="auto";
-static const auto vBool="bool";
-static const auto vCurrency="currency";
-static const auto vDate="date";
-static const auto vDateTime="datetime";
-static const auto vDouble="double";
-static const auto vInt="int";
-static const auto vNumber="number";
-static const auto vNumeric="numeric";
-static const auto vString="string";
-static const auto vText="text";
-static const auto vTime="time";
-static const auto vUrl="url";
-static const auto vUuid="uuid";
-static const EditReadOnly vReadOnly;
-
-static const auto vaCenter="center";
-static const auto vaEnd="end";
-static const auto vaStart="start";
-
-static const auto vlHorizontal="horizontal";
-static const auto vlVertical="vertical";
-
-static const auto vpActions="actions";
-static const auto vpAlign="align";
-static const auto vpColor="color";
-static const auto vpDataType="dataType";
-static const auto vpDefault="default";
-static const auto vpDefaultSelect="defaultSelect";
-static const auto vpDefaultValue="defaultValue";
-static const auto vpDesign="design";
-static const auto vpDisplayer="displayer";
-static const auto vpDisplayText="displaytext";
-static const auto vpEdit="edit";
-static const auto vpEditable="editable";
-static const auto vpField="field";
-static const auto vpFilters="filters";
-static const auto vpFilterStyle="filterStyle";
-static const auto vpFiltrable="filtrable";
-static const auto vpFiltrableStrategy="searchStrategy";
-static const auto vpFlags="flags";
-static const auto vpFont="font";
-static const auto vpFormat="format";
-static const auto vpHeaders="headers";
-static const auto vpHeight="height";
-static const auto vpId="id";
-static const auto vpInputFormat="inputFormat";
-static const auto vpInputMask="inputMask";
-static const auto vpInputType="inputType";
-static const auto vpInputLink="inputLink";
-static const auto vpItems="items";
-static const auto vpEndPoint="endpoint";
-static const auto vpEndPoints="endpoints";
-static const auto vpLayout="layout";
-static const auto vpLength="length";
-static const auto vpLinks="links";
-static const auto vpMethod="method";
-static const auto vpName="name";
-static const auto vpObrigatory="obrigatory";
-static const auto vpOptions="options";
-static const auto vpOrder="order";
-static const auto vpPerfumery="perfumerys";
-static const auto vpReadOnly="readOnly";
-static const auto vpResultInfo="resultInfo";
-static const auto vpColumns="columns";
-static const auto vpRows="rows";
-static const auto vpSearch="search";
-static const auto vpSortable="sortable";
-static const auto vpSortBy="sort-by";
-static const auto vpSortDesc="sort-desc";
-static const auto vpTitle="title";
-static const auto vpType="type";
-static const auto vpUuid="uuid";
-static const auto vpVisible="visible";
-static const auto vpWidth="width";
-static const auto vpPrimaryKey="primarykey";
-
-
-static const auto vtInputAddress="inputaddress";
-static const auto vtInputCalendar="inputcalendar";
-static const auto vtInputCheckBox="inputcheckbox";
-static const auto vtInputComboBox="inputcombobox";
-static const auto vtInputCurrency="inputcurrency";
-static const auto vtInputDate="inputdate";
-static const auto vtInputDateTime="inputdatetime";
-static const auto vtInputWeight="inputweight";
-static const auto vtInputDouble="inputdouble";
-static const auto vtInputInt="inputint";
-static const auto vtInputMail="inputemail";
-static const auto vtInputMd5="inputmd5";
-static const auto vtInputNumber="inputnumber";
-static const auto vtInputPhone="inputphone";
-static const auto vtInputRadioBox="inputradiobox";
-static const auto vtInputText="inputtext";
-static const auto vtInputTextArea="inputtextarea";
-static const auto vtInputTextMask="inputtextmask";
-static const auto vtInputTextSearch="inputtextsearch";
-static const auto vtInputTextWeight="inputtextweight";
-static const auto vtInputTime="inputtime";
-static const auto vtInputUuid="inputuuid";
-
-static const auto vpOutput="output";
-static const auto vpRoute="route";
-static const auto vpPath="path";
 
 static const auto driver_QMYSQL="QMYSQL";
 static const auto driver_QODBC="QODBC";
